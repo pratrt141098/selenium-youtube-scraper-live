d605db51c380051db26b92599a18333b3168fb90
pratrt141098 <63106060+pratrt141098@users.noreply.github.com>
2024-08-19 06:10:02 +0000

    changed email function by using MIME

--- scraper.py
+++ scraper.py
@@ -1,8 +1,18 @@
+import smtplib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common import by
+import os
 import pandas as pd
+import json
+from datetime import datetime
+import pytz
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+#from email.message import EmailMessage
+
 
 YOUTUBE_TRENDING_URL = "https://www.youtube.com/feed/trending"
 
@@ -52,11 +62,52 @@ def parse_video(video):
     'Thumbnail': thumbnail_url
   }
 
+def send_email(path, reciever):
+  try:
+    # updating time and date of scraping
+    currentDateAndTime = datetime.now(pytz.timezone('Asia/Kolkata'))
+    currentDate = currentDateAndTime.strftime("%Y:%m:%d")
+    currentTime = currentDateAndTime.strftime("%H:%M:%S")
+        
+    #this is new email code
+    subject = "Top 10 Trending Videos on Yooutube"
+    body = "This is a summary of the top 10 trending videos on Youtube in India as of " + currentDate + " at " + currentTime 
+    sender_email = 'thierrytribhuwan@gmail.com'
+    recipient_email = "prat10000@gmail.com"
+    sender_password = os.environ['GOOGLE_PW']
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 465
+    # MIMEMultipart() creates a container for an email message that can hold
+    # different parts, like text and attachments and in next line we are
+    # attaching different parts to email container like subject and others.
+    message = MIMEMultipart()
+    message['Subject'] = subject
+    message['From'] = sender_email
+    message['To'] = recipient_email
+    body_part = MIMEText(body)
+    message.attach(body_part)
+
+    # section 1 to attach file
+    with open(path,'rb') as file:
+      # Attach the file with filename to the email
+      message.attach(MIMEApplication(file.read(), Name="trending.csv"))
+
+      # secction 2 for sending email
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+      server.login(sender_email, sender_password)
+      server.sendmail(sender_email, recipient_email, message.as_string())
+      server.close()
+      print('Email sent successfully')
+    
+  except:
+    print("Something went wrong...")
+    
+  
 
 if __name__ == "__main__":
   print("Creating driver")
   driver = get_driver()
-  
+
   print("Fetching trending videos")
   videos = get_videos(driver)
   print(f'Found {len(videos)} videos')
@@ -68,7 +119,6 @@ if __name__ == "__main__":
   videos_df = pd.DataFrame(videos_data)
   print(videos_df)
   videos_df.to_csv('trending.csv')
-
-  
-  
-  
+  print('Sending an email with the results in a csv file')
+  send_email('trending.csv', 'prat10000@gmail.com')
+  print('Done')
